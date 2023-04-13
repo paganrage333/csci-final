c15120c0fb855b77ddb8f5c297e305ce9094798d
page <aragan@unca.edu>
2023-04-12 21:34:58 -0400

    formatting

--- final/sketch.js
+++ final/sketch.js
@@ -98,6 +98,26 @@ const c2 = {
     color: 'hotpink'
 };
 
+function happyFace (x, y, diam) {
+    // Face
+    fill(255, 255, 0);
+    stroke(0);
+    strokeWeight(2);
+    ellipse(x, y, diam, diam);
+    
+    // Smile
+    var startAng = .1*PI
+    var endAng = .9*PI
+    var smileDiam = .6*diam;
+    arc(x, y, smileDiam, smileDiam, startAng, endAng);
+    
+    // Eyes
+    var offset = .2*diam;
+    var eyeDiam = .1*diam;
+    fill(0);
+    ellipse(x-offset, y-offset, eyeDiam, eyeDiam);
+    ellipse(x+offset, y-offset, eyeDiam, eyeDiam);
+}
 
 
 // function drawCar(x1, y1, size, fillColor, wheelColor='black') {
